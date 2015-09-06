a552a4aa43ac33832f2029ac1ed97585f827b7eb
Jonathan Porta <jonathan@jonathanPorta.com>
2015-09-05 21:00:30 -0700

    Update ps scripts

--- powershell/chef-solo.ps1
+++ powershell/chef-solo.ps1
@@ -5,5 +5,5 @@ $chefRunList = Join-Path $chefDir 'run_list.json'
 Write-Host "Preparing to run chef-solo using config from $chefConfig using run list from $chefRunList..."
 Write-Host "Running: "
 Write-Host "chef-solo -c $chefConfig -j $chefRunList"
-
-(Start-Process -FilePath "chef-solo" -ArgumentList "-c $chefConfig -j $chefRunList" -Wait -Passthru).ExitCode
+chef-solo -c C:\ogs\windows-bootstrap-master\chef-solo\solo.rb -j C:\ogs\windows-bootstrap-master\chef-solo\run_list.json
+#(Start-Process -FilePath "chef-solo" -ArgumentList "-c $chefConfig -j $chefRunList" -Wait -Passthru).ExitCode
