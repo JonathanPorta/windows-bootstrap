5409881c6952f9f3bceaedf7d61d5b287c11ab89
Jonathan Porta <jonathan@jonathanPorta.com>
2015-09-05 18:58:11 -0700

    Update ps scripts

--- powershell/chef-repo.ps1
+++ powershell/chef-repo.ps1
@@ -1,19 +1,22 @@
 $tempDir = 'C:\tmp'
 if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
 
-$gitUrl = 'https://github.com/JonathanPorta/rust-server-cookbook.git'
-$repoPath = 'C:\cookbooks\rust'
-$repoRef = 'master'
+$startingDir = $PWD
+$cookbookUrl = 'https://github.com/JonathanPorta/rust-server-cookbook.git'
+$cookbookPath = 'C:\cookbooks\rust'
+$cookbookRef = 'master'
 
 # Clone the repository
-Write-Host "Cloning $gitUrl into $repoPath"
-git clone $gitUrl $repoPath
+Write-Host "Cloning $cookbookUrl into $cookbookPath"
+git clone $cookbookUrl $cookbookPath
 
-Write-Host "Reseting repository in $repoPath to origin/$repoRef"
-cd $repoPath
+Write-Host "Reseting repository in $cookbookPath to origin/$cookbookRef"
+cd $cookbookPath
 git fetch origin
-git reset --hard origin/$repoRef
+git reset --hard origin/$cookbookRef
+cd $startingDir
 
 Write-Host "Installing cookbook dependencies..."
-cd $repoPath
+cd $cookbookPath
 berks install
+cd $startingDir
