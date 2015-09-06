db7c1cf48e276aa3d7c38bbf65b02448e1f33642
Jonathan Porta <jonathan@jonathanPorta.com>
2015-09-05 19:21:10 -0700

    Update ps scripts

--- powershell/chef-installer.ps1
+++ powershell/chef-installer.ps1
@@ -2,16 +2,18 @@
 
 # Download the chef-solo installer
 $chefSoloInstaller = Join-Path $tempDir 'chef-solo.msi'
-Write-Host "Downloading chef-solo MSI to $chefSoloInstaller"
+Write-Host "Downloading chef-solo MSI to $chefSoloInstaller..."
 Download-File 'https://www.opscode.com/chef/install.msi' "$chefSoloInstaller"
 
 # Execute the installer and pass the exit code through to the shell
+Write-Host "Running the chef-solo installer..."
 (Start-Process -FilePath "msiexec.exe" -ArgumentList "/package $chefSoloInstaller /passive" -Wait -Passthru).ExitCode
 
 # Download the chef-dk installer
 $chefDkInstaller = Join-Path $tempDir 'chef-solo.msi'
-Write-Host "Downloading chef-dk MSI to $chefDkInstaller"
+Write-Host "Downloading chef-dk MSI to $chefDkInstaller..."
 Download-File 'https://opscode-omnibus-packages.s3.amazonaws.com/windows/2008r2/x86_64/chefdk-0.4.0-1.msi' "$chefDkInstaller"
 
 # Execute the installer and pass the exit code through to the shell
+Write-Host "Running the chef-dk installer..."
 (Start-Process -FilePath "msiexec.exe" -ArgumentList "/package $chefDkInstaller /passive" -Wait -Passthru).ExitCode
